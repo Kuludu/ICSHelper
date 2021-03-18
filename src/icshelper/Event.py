2477f9a0008d7dffb3133e87df86a47e9ff04bce
Kuludu <i@kuludu.net>
2021-03-18 22:40:01 +0800

    Update Calendar.py

---
+++ src/icshelper/Event.py
@@ -0,0 +1,8 @@
+class Event:
+    event = dict()
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
---
+++ src/icshelper/Util.py
@@ -0,0 +1,5 @@
+import Event
+
+
+def genEventsByWeeks():
+    pass
